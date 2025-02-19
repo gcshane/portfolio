5575a723c8efdb0159d5534edcd8ab7ec92eff57
gcshane <gcshane@gmail.com>
2025-02-20 00:32:44 +0800

    Change header effect

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 let i = 0;
 let j = 0;
-let txt = [" Software Engineer", " Full-Stack Developer"];
+let txt = "Software Engineer";
 let forward = true
 let target = document.getElementById("engineer");
 
 function typeWriter() {
 
-    if (i == txt[j].length && forward) {
+    if (i == txt.length && forward) {
         forward = false;
     }
     else if (i == 0 && ! forward) {
         forward = true;
-        j = j ^ 1;
     }
 
     if (forward) {
-        target.innerHTML += txt[j].charAt(i);
+        target.innerHTML += txt.charAt(i);
         i++;
     }
     else {
         target.innerHTML = target.innerHTML.slice(0, -1);
         i--;
     }
-    if (i == txt[j].length) {
-        setTimeout(typeWriter, 1500);
+    if (i == txt.length) {
+        setTimeout(typeWriter, 1000);
     }
     else {
-        setTimeout(typeWriter, 150);
+        setTimeout(typeWriter, 50);
     }
 
 }
